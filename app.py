0257bb07c6f243630ca6f1994c180c52ebdfced7
jaredleys <jaredleys@gmail.com>
2021-07-18 11:25:16 -0400

    added Flash template

--- app.py
+++ app.py
@@ -13,12 +13,9 @@ def home():
     return render_template("index.html", mars=mars_data)
 
 @app.route("/map")
+def map():
 
-@app.route("/scrape")
-def scrape():
-    mars_info = scrape_mars.scrape()
-    mongo.db.collection.update({},mars_info,upsert=True)
-    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
