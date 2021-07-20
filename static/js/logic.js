e402813117b6e0e0c1b9515bd4f3a6619c41ba77
Jamin Hershberger <jaminhershberger@Jamins-MacBook-Pro.local>
2021-07-20 19:22:23 -0400

    Adding fade in fade out title

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,48 +1,70 @@
 // Create the tile layer that will be the background of our map
 var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "light-v10",
-  accessToken: API_KEY
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
 });
 
 // Initialize all of the LayerGroups we'll be using
 var layers = {
     Tornadoes: new L.LayerGroup(),
     Wildfires: new L.LayerGroup()
-  };
-  
-  // Create the map with our layers
-  var myMap = L.map("map-id", {
-    center: [39.82, -98.57],
-    zoom:5,
-    layers: [
-      layers.Tornadoes,
-      layers.Wildfires
-    ]
-  });
+};
 
-  // Add our 'lightmap' tile layer to the map
+// Create the map with our layers
+var myMap = L.map("map-id", {
+    center: [39.82, -98.57],
+    zoom: 5,
+    layers: [
+        layers.Tornadoes,
+        layers.Wildfires
+    ]
+});
+
+// Add our 'lightmap' tile layer to the map
 lightmap.addTo(myMap);
 
 // Create an overlays object to add to the layer control
 var overlays = {
-  "Tornadoes": layers.Tornadoes,
-  "Wildfires": layers.Wildfires
+    "Tornadoes": layers.Tornadoes,
+    "Wildfires": layers.Wildfires
 };
 
 // Create a control for our layers, add our overlay layers to it
 L.control.layers(null, overlays).addTo(myMap);
 
 var info = L.control({
-  position: "bottomright"
+    position: "bottomright"
 });
 
 // When the layer control is added, insert a div with the class of "legend"
 info.onAdd = function() {
-  var div = L.DomUtil.create("div", "legend");
-  return div;
+    var div = L.DomUtil.create("div", "legend");
+    return div;
 };
 
 // Add the info legend to the map
 info.addTo(myMap);
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml3 .letter',
+        opacity: [0, 4],
+        easing:
+
+            "easeInOutQuad",
+        duration: 2250,
+        delay: (el, i) => 500 * (i + 1)
+    }).add({
+        targets: '.ml3',
+        opacity: 100,
+        duration: 400000,
+        easing: "easeOutExpo",
+        delay: 100
+    });
