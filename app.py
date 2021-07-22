03c2332b1c91103b48dc23f96850dec64ef706f1
jaredleys <jaredleys@gmail.com>
2021-07-22 16:50:34 -0400

    fixed bugs in app

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from models import create_earthquakes, create_tornados
+import models
 from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import os
 from config import password
 
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:{password}@localhost:5432/disaster_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-earthquakes = create_earthquakes(db)
-tornados = create_tornados(db)
-print(earthquakes)
 
 @app.route("/")
 def home():
@@ -17,8 +15,8 @@ def home():
 
 @app.route("/map")
 def map():
-    e_results = db.session.query(earthquakes['lat'], earthquakes['lon'], earthquakes['mag'], earthquakes['year'], earthquakes['date']).all()
-    t_results = db.session.query(tornados['lat'], tornados['lon'], tornados['mag'], tornados['year'], tornados['date']).all()
+    e_results = db.session.query(models.Earthquakes.lat, models.Earthquakes.lon, models.Earthquakes.mag, models.Earthquakes.year, models.Earthquakes.date).all()
+    t_results = db.session.query(models.Tornados.lat, models.Tornados.lon, models.Tornados.mag, models.Tornados.year, models.Tornados.date).all()
     
     e_lat = [result[0] for result in e_results]
     e_lon = [result[1] for result in e_results]
--- models.py
+++ models.py
@@ -1,45 +1,45 @@
-def create_earthquakes(db):
-    class Earthquakes(db.Model):
-        __tablename__ = 'earthquake'
+from app import db
 
-        index = db.Column(db.Integer, primary_key=True)
-        lat = db.Column(db.Float())
-        lon = db.Column(db.Float())
-        mag = db.Column(db.Float())
-        date = db.Column(db.Date())
-        year = db.Column(db.Integer())
+class Earthquakes(db.Model):
+    __tablename__ = 'earthquake'
 
-        # def __init__(self, lat, lon, mag, date, year, result_all):
-            
-        #     self.lat = lat
-        #     self.lon = lon
-        #     self.mag = mag
-        #     self.date = date
-        #     self.year = year
-        #     self.result_all = result_all
+    index = db.Column(db.Integer, primary_key=True)
+    lat = db.Column(db.Float())
+    lon = db.Column(db.Float())
+    mag = db.Column(db.Float())
+    date = db.Column(db.Date())
+    year = db.Column(db.Integer())
 
-        def __repr__(self):
-            return '<id {}>'.format(self.id)
+    # def __init__(self, lat, lon, mag, date, year, result_all):
+        
+    #     self.lat = lat
+    #     self.lon = lon
+    #     self.mag = mag
+    #     self.date = date
+    #     self.year = year
+    #     self.result_all = result_all
 
-def create_tornados(db):
-    class Tornados(db.Model):
-        __tablename__ = 'tornado'
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
 
-        index = db.Column(db.Integer, primary_key=True)
-        lat = db.Column(db.Float())
-        lon = db.Column(db.Float())
-        mag = db.Column(db.Integer())
-        date = db.Column(db.Date())
-        year = db.Column(db.Integer())
+class Tornados(db.Model):
+    __tablename__ = 'tornado'
 
-        # def __init__(self, lat, lon, mag, date, year, result_all):
-            
-        #     self.lat = lat
-        #     self.lon = lon
-        #     self.mag = mag
-        #     self.date = date
-        #     self.year = year
-        #     self.result_all = result_all
+    index = db.Column(db.Integer, primary_key=True)
+    lat = db.Column(db.Float())
+    lon = db.Column(db.Float())
+    mag = db.Column(db.Integer())
+    date = db.Column(db.Date())
+    year = db.Column(db.Integer())
 
-        def __repr__(self):
-            return '<id {}>'.format(self.id)
+    # def __init__(self, lat, lon, mag, date, year, result_all):
+        
+    #     self.lat = lat
+    #     self.lon = lon
+    #     self.mag = mag
+    #     self.date = date
+    #     self.year = year
+    #     self.result_all = result_all
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
