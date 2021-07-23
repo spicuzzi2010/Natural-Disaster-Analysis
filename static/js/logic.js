648aa943587e395a6e5657864548c2c17785c2ec
Douglas Krug <dkrug82@gmail.com>
2021-07-22 20:12:51 -0400

    moved ETL to cleaning data file and wrote cleaned data to csv

--- static/js/logic.js
+++ static/js/logic.js
@@ -53,6 +53,7 @@ info.onAdd = function() {
 // Add the info legend to the map
 info.addTo(myMap);
 
+
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml3');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
