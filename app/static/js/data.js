dfef34965e649abb9269a891cbe716cb544eb20e
jaredleys <jaredleys@gmail.com>
2021-07-28 17:48:46 -0400

    calling data differently

--- app/static/js/data.js
+++ app/static/js/data.js
@@ -1,4 +1,4 @@
-const url = "/api/disasters";
-function saveData(data) {disasterData = data};
-d3.json(url).then(saveData);
-console.log(disasterData);
+// const url = "/api/disasters";
+// function saveData(data) {disasterData = data};
+// d3.json(url).then(saveData);
+// console.log(disasterData);
--- app/static/js/logic.js
+++ app/static/js/logic.js
@@ -1,3 +1,6 @@
+const url = "/api/disasters";
+d3.json(url).then(disasterData => {
+
 function createMap(layers) {
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -198,3 +201,4 @@ anime.timeline({ loop: true })
 
 var updateButton = d3.select("#update-button");
 updateButton.on("click", statePoints(disasterData));
+});
