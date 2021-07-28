9964718f601d47b637ce93fc16eb9c125d4eef34
jaredleys <jaredleys@gmail.com>
2021-07-28 18:05:14 -0400

    returning to logic

--- app/static/js/logic.js
+++ app/static/js/logic.js
@@ -1,6 +1,3 @@
-const url = "/api/disasters";
-d3.json(url).then(disasterData => {
-
 function createMap(layers) {
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -176,7 +173,8 @@ function runData(disasters) {
     statePoints(disasterData)
 };
 
-runData(disasterData)
+const url = "/api/disasters";
+d3.json(url).then(runData)
 
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml3');
@@ -200,5 +198,5 @@ anime.timeline({ loop: true })
     });
 
 var updateButton = d3.select("#update-button");
-updateButton.on("click", statePoints(disasterData));
-});
+updateButton.on("click", d3.json(url).then(statePoints));
+
